c7b39cedfbdf838948ac83d8e5c0e2ffd07543de
whatl3y <whatl3y@gmail.com>
2024-02-29 13:39:32 -0500

    update keeper compatible interface to use automation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -40,22 +40,22 @@ async function main() {
     // '0x1F98431c8aD98523631AE4a59f267346ea31F984',
     // '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
     // LendingPool
-    // 'https://api.hyperbolicprotocol.com/loans/metadata/',
-    // '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    // '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA',
-    // '0xdf9a6debb35be847d6addb7843e763539671b2c7',
-    // '0x300648a601c584E6379fa0D6a31CbBFCcA6177e4',
-    // '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+    'https://api.hyperbolicprotocol.com/loans/arbitrum/metadata/',
+    '0xE592427A0AEce92De3Edee1F18E0157C05861564',
+    '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA',
+    '0xdf9a6debb35be847d6addb7843e763539671b2c7',
+    '0x300648a601c584E6379fa0D6a31CbBFCcA6177e4',
+    '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
     // LendingPoolExtLP
-    // 'https://api.hyperbolicprotocol.com/loans/ext/metadata/',
+    // 'https://api.hyperbolicprotocol.com/loans/arbitrum/ext/metadata/',
     // '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     // '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA',
     // '0xdf9a6debb35be847d6addb7843e763539671b2c7',
     // '0x300648a601c584E6379fa0D6a31CbBFCcA6177e4',
     // '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-    // CCIPBridge
-    '0x141fa059441E0ca23ce184B6A78bafD2A517DdE8',
-    '0x77B4d8cB74Ae7C72fbD311A9a13d53baAc049094'
+    // // CCIPBridge
+    // '0x141fa059441E0ca23ce184B6A78bafD2A517DdE8',
+    // '0x77B4d8cB74Ae7C72fbD311A9a13d53baAc049094'
 
     // eth
     // '0xBf1858b24243Ecbcf7d940f458e36CB7401c2366',
@@ -64,14 +64,14 @@ async function main() {
     // '0x1F98431c8aD98523631AE4a59f267346ea31F984',
     // '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     // LendingPool
-    // 'https://api.hyperbolicprotocol.com/loans/metadata/',
+    // 'https://api.hyperbolicprotocol.com/loans/arbitrum/metadata/',
     // '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     // '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA',
     // '0xBf1858b24243Ecbcf7d940f458e36CB7401c2366',
     // '0x300648a601c584E6379fa0D6a31CbBFCcA6177e4',
     // '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     // LendingPoolExtLP
-    // 'https://api.hyperbolicprotocol.com/loans/ext/metadata/',
+    // 'https://api.hyperbolicprotocol.com/loans/arbitrum/ext/metadata/',
     // '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     // '0x85225Ed797fd4128Ac45A992C46eA4681a7A15dA',
     // '0xBf1858b24243Ecbcf7d940f458e36CB7401c2366',
